fdf570dcb1839051d2f0d692c811708d1e9cf58f
SuperLlama <superllama527@hotmail.com>
2012-12-27 16:06:06 -0500

    apparently you have to put a message in here >.>

---
+++ lua/gravityhull/sh_entityoverrides.lua
@@ -0,0 +1,317 @@
+local GH = GravHull
+local ENT = FindMetaTable("Entity")
+
+------------------------------------------------------------------------------------------
+-- Name: GetRealPos
+-- Desc: Save the original function elsewhere.
+------------------------------------------------------------------------------------------
+if !ENT.GetRealPos then
+    ENT.GetRealPos = ENT.GetPos
+end
+
+------------------------------------------------------------------------------------------
+-- Name: GetRealAngles
+-- Desc: Save the original function elsewhere.
+------------------------------------------------------------------------------------------
+if !ENT.GetRealAngles then
+	ENT.GetRealAngles = ENT.GetAngles
+end
+
+------------------------------------------------------------------------------------------
+-- Name: RealFireBullets
+-- Desc: Save the original function elsewhere.
+------------------------------------------------------------------------------------------
+if !ENT.RealFireBullets then
+	ENT.RealFireBullets = ENT.FireBullets
+end
+
+------------------------------------------------------------------------------------------
+-- Name: GetRealVelocity
+-- Desc: Save the original function elsewhere.
+------------------------------------------------------------------------------------------
+if !ENT.GetRealVelocity then
+	ENT.GetRealVelocity = ENT.GetVelocity
+end
+
+------------------------------------------------------------------------------------------
+-- Name: FireBullets
+-- Desc: Also fire bullets from the physghost.
+------------------------------------------------------------------------------------------
+ENT.FireBullets = function(self,tbl)
+	if !GH.BulletGhost then
+		GH.BulletGhost = ents.Create("prop_physics")
+		GH.BulletGhost:SetModel("models/hunter/plates/plate.mdl")
+		GH.BulletGhost:SetColor(Color(0,0,0,1))
+		if SERVER then GH.BulletGhost:SetNoDraw(true) end
+		GH.BulletGhost:Spawn()
+		if SERVER then GH.BulletGhost:GetPhysicsObject():EnableCollisions(false) end
+	end
+	local data,s,g
+	if SERVER then
+		if self:IsWeapon() and IsValid(self:GetOwner()) and self:GetOwner().InShip then
+			s = self:GetOwner().InShip
+			data = GH.SHIPS[self:GetOwner().InShip]
+		elseif self.InShip or self.MyShip then
+			s = self.InShip
+			data = GH.SHIPS[self.InShip or self.MyShip]
+		end
+		if data then g = data.MainGhost end
+	else
+		if self:IsWeapon() and IsValid(self:GetOwner()) and GH.SHIPCONTENTS[self:GetOwner()] then
+			data = GH.SHIPCONTENTS[self:GetOwner()]
+		elseif GH.SHIPCONTENTS[self] then
+			data = GH.SHIPCONTENTS[self]
+		end
+		if data then
+			s = data.S
+			g = data.G
+		end
+	end
+	if (IsValid(s) && IsValid(g)) then
+		local ttbl = table.Copy(tbl)
+		local rtran = false
+		if tbl.Src:Distance(self:GetPos())/2 < ((self:IsWeapon() and self:GetOwner():BoundingRadius()) or self:BoundingRadius()) then
+			s,g = g,s
+			rtran = true
+		end
+		--src transform
+		local pos,ang = WorldToLocal(ttbl.Src,Angle(0,0,0),g.RealPos or g:GetRealPos(),g.RealAng or g:GetRealAngles())
+		pos,ang = LocalToWorld(pos,ang,s.RealPos or s:GetRealPos(),s.RealAng or s:GetRealAngles())
+		ttbl.Src = pos
+		--dir transform
+		pos,ang = WorldToLocal(ttbl.Dir,Angle(0,0,0),vector_origin,g.RealAng or g:GetRealAngles())
+		pos,ang = LocalToWorld(pos,ang,vector_origin,s.RealAng or s:GetRealAngles())
+		ttbl.Dir = pos		
+		if SERVER and !game.SinglePlayer() then tbl.Tracer, ttbl.Tracer = nil, nil end
+		if rtran then
+			tbl.Attacker = (tbl.Attacker or (IsValid(self:GetOwner()) and self:GetOwner()) or self)
+			if SERVER then
+				GH.BulletGhost:SetRealPos(tbl.Src)
+			else
+				GH.BulletGhost:SetPos(ttbl.Src)
+			end
+			GH.BulletGhost.Inflictor = (self:IsPlayer() and self:GetActiveWeapon()) or self
+			GH.BulletGhost:RealFireBullets(tbl)
+			self:RealFireBullets(ttbl)
+		else
+			ttbl.Attacker = (ttbl.Attacker or (IsValid(self:GetOwner()) and self:GetOwner()) or self)
+			if SERVER then
+				GH.BulletGhost:SetRealPos(ttbl.Src)
+			else
+				GH.BulletGhost:SetPos(ttbl.Src)
+			end
+			GH.BulletGhost.Inflictor = (self:IsPlayer() and self:GetActiveWeapon()) or self
+			GH.BulletGhost:RealFireBullets(ttbl)
+			self:RealFireBullets(tbl)
+		end
+	else
+		self:RealFireBullets(tbl)
+		if SERVER then
+			local tr = util.RealTraceLine{start = tbl.Src, endpos = tbl.Src + tbl.Dir*16384, filter = self}
+			if !IsValid(tr.Entity) then return end
+			local ship = ((GH.PHYSGHOSTS[tr.Entity] and GH.PHYSGHOSTS[tr.Entity].InShip) or (tr.Entity.Ghost and tr.Entity.Ghost.MyShip))
+			if GH.SHIPS[ship] then
+				local ttbl = table.Copy(tbl)
+				local ghost = GH.SHIPS[ship].MainGhost
+				--src transform
+				local pos,ang = WorldToLocal(ttbl.Src,Angle(0,0,0),ship:GetPos(),ship:GetAngles())
+				pos,ang = LocalToWorld(pos,ang,ghost:GetRealPos(),ghost:GetRealAngles())
+				ttbl.Src = pos
+				--dir transform
+				pos,ang = WorldToLocal(ttbl.Dir,Angle(0,0,0),vector_origin,ship:GetAngles())
+				pos,ang = LocalToWorld(pos,ang,vector_origin,ghost:GetRealAngles())
+				ttbl.Dir = pos
+				if !game.SinglePlayer() then ttbl.Tracer = nil end
+				ttbl.Attacker = (ttbl.Attacker or (IsValid(self:GetOwner()) and self:GetOwner()) or self)
+				GH.BulletGhost:SetRealPos(ttbl.Src)
+				GH.BulletGhost.Inflictor = (self:IsPlayer() and self:GetActiveWeapon()) or self
+				GH.BulletGhost:RealFireBullets(ttbl)
+			end
+		end
+	end
+end
+
+------------------------------------------------------------------------------------------
+-- Name: GetPos
+-- Desc: Override the function.
+------------------------------------------------------------------------------------------
+ENT.GetPos = function(self,_nomr)
+	if GH.DebugOverride and !IsValid(self) then error("Tried to use a NULL entity!",2) end
+    self.UsedFakePos = true
+	local nomr = _nomr
+	if MapRepeat and MapRepeat.PosWrap and MapRepeat.PosWrap > 0 then nomr = true end
+	if self.FakePosTime == CurTime() then return self.FakePos end
+    if SERVER and (IsValid(self.InShip or self.MyShip) and GH.SHIPS[self.InShip or self.MyShip]) or
+       CLIENT and (GH.SHIPCONTENTS[self] or GH.GHOSTHULLS[self]) then
+        if self:IsWeapon() then
+            return self:GetRealPos()
+        else
+            local data = SERVER and (GH.SHIPS[self.InShip or self.MyShip]) or
+                         CLIENT and (GH.SHIPCONTENTS[self] or GH.GHOSTHULLS[self])
+            local s,g = (self.InShip or self.MyShip or data.S), (data.MainGhost or data.G)
+			if (IsValid(s) && IsValid(g)) then
+				local pos,ang = WorldToLocal(self.RealPos or self:GetRealPos(),self.RealAng or self:GetRealAngles(),g.RealPos or g:GetRealPos(),g.RealAng or g:GetRealAngles())
+				pos,ang = LocalToWorld(pos,ang,s:GetPos(nomr),s:GetAngles()) --recursive so you can live out your inception fantasies
+				self.FakePos = pos
+				self.FakePosTime = CurTime()
+				return pos or self:GetRealPos()
+			else
+				return self:GetRealPos()
+			end
+        end
+    elseif SERVER and !nomr and MapRepeat and self.Cells and #self.Cells == 1 then
+		return MapRepeat.CellToPos(self:GetRealPos(),self.Cells[1])
+	elseif CLIENT and !nomr and MapRepeat and type(MapRepeat.CelledEnts[self]) == 'string' then
+		return MapRepeat.CellToPos(self:GetRealPos(),MapRepeat.CelledEnts[self])
+	else
+		return self:GetRealPos()
+    end
+end
+
+
+------------------------------------------------------------------------------------------
+-- Name: SetPos
+-- Desc: Override the SetPos function; required for proper teleportation.
+------------------------------------------------------------------------------------------
+if !ENT.SetRealPos then
+	ENT.SetRealPos = ENT.SetPos
+end
+ENT.SetPos = function(self,pos,_nomr)
+	local nomr = _nomr
+	if MapRepeat and MapRepeat.PosWrap and MapRepeat.PosWrap > 0 then nomr = true end
+	if SERVER then
+		if IsValid(self.InShip) and GH.SHIPS[self.InShip] then
+			local S,G = self.InShip,GH.SHIPS[self.InShip].MainGhost
+			if IsValid(S) && IsValid(G) then
+				local npos = WorldToLocal(pos,Angle(0,0,0),S:GetPos(),S:GetAngles())
+				npos = LocalToWorld(npos,Angle(0,0,0),G:GetRealPos(),G:GetRealAngles())
+				self:SetRealPos(npos)
+			else
+				self:SetRealPos(pos)
+			end
+		else
+			if GH.DebugOverride and !IsValid(self) then error("Tried to use a NULL entity!",2) end
+			if MapRepeat then
+				local cell,npos = MapRepeat.PosToCell(pos)
+				if !(self.Cells and cell == self.Cells[1]) then
+					MapRepeat.SetCell(self,cell)
+				end
+				self:SetRealPos(npos)
+			else
+				self:SetRealPos(pos)
+			end
+		end
+	elseif MapRepeat and !nomr then
+		local cell,npos = MapRepeat.PosToCell(pos)
+		self:SetRealPos(npos)
+	else
+		self:SetRealPos(pos)
+	end
+end
+
+------------------------------------------------------------------------------------------
+-- Name: GetAngles
+-- Desc: Override the function.
+------------------------------------------------------------------------------------------
+ENT.GetAngles = function(self)
+	if GH.DebugOverride and !IsValid(self) then error("Tried to use a NULL entity!",2) end
+	self.UsedFakeAng = true
+	if self.FakeAngTime == CurTime() then return self.FakeAng end
+    if SERVER and (IsValid(self.InShip or self.MyShip) and GH.SHIPS[self.InShip or self.MyShip]) or
+       CLIENT and (GH.SHIPCONTENTS[self] or GH.GHOSTHULLS[self]) then
+        if self:IsWeapon() then
+            return self:GetRealPos()
+        else
+            local data = SERVER and (GH.SHIPS[self.InShip or self.MyShip]) or
+                         CLIENT and (GH.SHIPCONTENTS[self] or GH.GHOSTHULLS[self])
+            local s,g = (self.InShip or self.MyShip or data.S), (data.MainGhost or data.G)
+			if (IsValid(s) && IsValid(g)) then
+				local pos,ang = WorldToLocal(self.RealPos or self:GetRealPos(),self.RealAng or self:GetRealAngles(),g.RealPos or g:GetRealPos(),g.RealAng or g:GetRealAngles())
+				pos,ang = LocalToWorld(pos,ang,s:GetPos(),s:GetAngles())
+				self.FakeAng = ang
+				self.FakeAngTime = CurTime()
+				return ang or self:GetRealAngles()
+			else
+				return self:GetRealAngles()
+			end
+        end
+    else
+        return self:GetRealAngles()
+    end
+end
+
+
+------------------------------------------------------------------------------------------
+-- Name: GetVelocity
+-- Desc: Currently disabled...
+------------------------------------------------------------------------------------------
+/*
+ENT.GetVelocity = function(self)
+	self.UsedFakeVel = true
+	if self.FakeVelTime == CurTime() then return self.FakeVel end
+    if SERVER and (IsValid(self.InShip or self.MyShip) and GH.SHIPS[self.InShip or self.MyShip]) or
+       CLIENT and (GH.SHIPCONTENTS[self] or GH.GHOSTHULLS[self]) then
+		local data = SERVER and (GH.SHIPS[self.InShip or self.MyShip]) or
+					 CLIENT and (GH.SHIPCONTENTS[self] or GH.GHOSTHULLS[self])
+		local s,g = (self.InShip or self.MyShip or data.S), (data.MainGhost or data.G)
+		if (IsValid(s) && IsValid(g)) then
+			local vel = WorldToLocal(self:GetRealVelocity(),Angle(0,0,0),vector_origin,g.RealAng or g:GetRealAngles())
+			vel = LocalToWorld(vel,Angle(0,0,0),vector_origin,s:GetAngles())
+			self.FakeVel = vel
+			self.FakeVelTime = CurTime()
+			return vel or self:GetRealVelocity()
+		else
+			return self:GetRealVelocity()
+		end
+    else
+        return self:GetRealVelocity()
+    end
+end*/
+
+------------------------------------------------------------------------------------------
+-- Name: FindInSphere
+-- Desc: Override the function.
+------------------------------------------------------------------------------------------
+if !ents.RealFindInSphere then
+	ents.RealFindInSphere = ents.FindInSphere
+end
+ents.FindInSphere = function(vec,rad)
+	local out = {}
+	for _,e in pairs(ents.GetAll()) do
+		if IsValid(e) and e:GetPos():Distance(vec) <= rad and !e.SLIsGhost then
+			out[#out+1] = e
+		end
+	end
+	return out
+end
+
+
+if !ENT.RealWorldToLocal then
+	ENT.RealWorldToLocal = ENT.WorldToLocal
+end
+function ENT:WorldToLocal(pos)
+	local out,_ = WorldToLocal(pos,Angle(0,0,0),self:GetPos(),self:GetAngles())
+	return out
+end
+if !ENT.RealWorldToLocalAngles then
+	ENT.RealWorldToLocalAngles = ENT.WorldToLocalAngles
+end
+function ENT:WorldToLocalAngles(ang)
+	local _,out = WorldToLocal(vector_origin,ang,self:GetPos(),self:GetAngles())
+	return out
+end
+
+if !ENT.RealLocalToWorld then
+	ENT.RealLocalToWorld = ENT.LocalToWorld
+end
+function ENT:LocalToWorld(pos)
+	local out,_ = LocalToWorld(pos,Angle(0,0,0),self:GetPos(),self:GetAngles())
+	return out
+end
+if !ENT.RealLocalToWorldAngles then
+	ENT.RealLocalToWorldAngles = ENT.LocalToWorldAngles
+end
+function ENT:LocalToWorldAngles(ang)
+	local _,out = LocalToWorld(vector_origin,ang,self:GetPos(),self:GetAngles())
+	return out
+end
---
+++ lua/weapons/gmod_tool/stools/localphysics.lua
@@ -0,0 +1,75 @@
+table.Merge(TOOL,{
+	Name = "Gravity Hull Designator",
+	Category = "Construction",
+	ClientConVar = {
+		floordist = 0,
+		gravnormal = 0,
+		gravity = 100,
+	}
+})
+if CLIENT then
+	for k,v in pairs{
+		Tool_localphysics_name = "Gravity Hull Designator",
+		Tool_localphysics_desc = "Create a local physics system for a ship or building so that you can walk around inside regardless of its movement or angles.",
+		Tool_localphysics_0 = "Fire at an upright hull to create a local physics system. Alt-fire at a designated hull to remove the system.",
+		Hint_ghd = "Check your chatbox for instructions.",
+	} do 
+		language.Add(k,v)
+	end
+else
+	concommand.Add("ghd_help",function(p)
+		p:SendHint("ghd",0)
+		p:ChatPrint("==Gravity Hull Designator Help==")
+		p:ChatPrint("Use the tool on a contraption to create a hull.")
+		p:ChatPrint("If you want to walk on top of it (i.e. flat plate), increase the vertical protrusion factor in the tool rollout.")
+		p:ChatPrint("If you want to align gravity with the surface you shot, check the Hit Surface Defines Floor box in the tool rollout.")
+		p:ChatPrint("Change the Player Gravity Percentage slider to define how much gravity pulls on players. Doesn't work on props (YET).")
+	end)
+end
+function TOOL.BuildCPanel(cp)
+	cp:AddControl("Header",{Text = "#Tool_localphysics_name", Description = "#Tool_toolname_desc"})
+	cp:AddControl("Slider",{Label = "Vertical Protrusion Factor", Description = "The minimum distance from the floor that walls or a ceiling is required to keep entities inside.",
+	                        Type = "Integer", Min = 0, Max = 300, Command = "localphysics_floordist"})
+	cp:AddControl("Checkbox",{Label = "Hit Surface Defines Floor", Description = "If checked, the surface shot by the tool will determine the pull of gravity for the system.",
+							  Command = "localphysics_gravnormal"})
+	cp:AddControl("Slider",{Label = "Player Gravity Percentage", Description = "The percentage of normal gravity to apply to players inside. *DOES NOT WORK WITH PROPS YET*",
+							Type = "Integer", Min = 0, Max = 500, Command = "localphysics_gravity"})
+	cp:AddControl("Button",{Label = "Help", Description = "Help, obviously", Command = "ghd_help"})
+	cp:AddControl("Button",{Label = "Fix Camera", Description = "If you're teleporting to the sky when you enter a ship, click this until it works.", Command = "ghd_fixcamera"})
+end
+--Designate Hull
+function TOOL:LeftClick(tr)
+	local ent = tr.Entity
+	if CLIENT then return IsValid(ent) and !GravHull.GHOSTHULLS[ent] end
+	if !(IsValid(ent) and ent:GetMoveType() == MOVETYPE_VPHYSICS and !GravHull.HULLS[ent]) then return false end
+	GravHull.RegisterHull(ent,self:GetClientNumber("floordist"),self:GetClientNumber("gravity"))
+	if self:GetClientNumber("gravnormal") > 0 then
+		GravHull.UpdateHull(ent,tr.HitNormal)
+	else
+		GravHull.UpdateHull(ent)
+	end
+	self:GetOwner():ChatPrint("Created a local physics system.")
+	return true
+end
+--Remove Hull
+function TOOL:RightClick(tr)
+	local ent = tr.Entity
+	if CLIENT then return IsValid(ent) end
+	if IsValid(ent) and ent:GetMoveType() == MOVETYPE_VPHYSICS then
+		if !GravHull.SHIPS[ent] then
+			ent = ent.MyShip or (ent.Ghost and ent.Ghost.MyShip)
+		end
+	else
+		return false
+	end
+	if !IsValid(ent) then return false end
+	self:GetOwner():ChatPrint("Removed a local physics system.")
+	GravHull.UnHull(ent)
+	return true
+end
+function TOOL:Reload(tr)
+	//idk this is for later
+end
+function TOOL:Think(tr)
+	//idk this is for later
+end
